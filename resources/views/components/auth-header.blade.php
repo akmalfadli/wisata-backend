1c9a2563ac6f60db48c1a8130ce9bd8408780e16
Akmal Fadli <akmalfadli94@gmail.com>
2024-08-31 06:52:05 +0700

    ganti logo perwira dan style css

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,6 +1,5 @@
  <div class="login-brand">
      <img src="{{ asset('img/perwira.svg') }}"
          alt="logo"
-         width="100"
-         class="shadow-light rounded-circle">
+         width="150">
  </div>
