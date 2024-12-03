f250d306787f33fc8239cf3fb7247cebd1a73f8e
Akmal Fadli <akmalfadli94@gmail.com>
2024-12-04 05:41:51 +0700

    fix index title

--- resources/views/pages/finance/categories/index.blade.php
+++ resources/views/pages/finance/categories/index.blade.php
@@ -18,7 +18,7 @@
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Keuangan</a></div>
-                    <div class="breadcrumb-item">All Category</div>
+                    <div class="breadcrumb-item">Ketgori Keuangan</div>
                 </div>
             </div>
             <div class="section-body">
--- resources/views/pages/finance/credit/index.blade.php
+++ resources/views/pages/finance/credit/index.blade.php
@@ -18,7 +18,7 @@
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Keuangan</a></div>
-                    <div class="breadcrumb-item">Credit</div>
+                    <div class="breadcrumb-item">Uang Masuk</div>
                 </div>
             </div>
             <div class="section-body">
